592592696fa43be860c6ee92ea21ce742f6c4250
Rafael Passos Guimarães <rapassos@gmail.com>
2026-06-11 16:39:34 -0300

    test: include initial application context loading test

---
+++ src/test/java/com/rapassos/smart_production_planner/SmartProductionPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapassos.smart_production_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartProductionPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
