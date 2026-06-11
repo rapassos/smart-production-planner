d584c2917c99942e506980570ac9ace368866946
Rafael Passos Guimarães <rapassos@gmail.com>
2026-06-11 16:37:31 -0300

    feat(infra): add main application class and maven wrapper configurations

---
+++ src/main/java/com/rapassos/smart_production_planner/SmartProductionPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.rapassos.smart_production_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartProductionPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartProductionPlannerApplication.class, args);
+	}
+
+}
